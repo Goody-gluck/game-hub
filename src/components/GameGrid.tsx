6dec5e0b9d4ac99a1e5917bc80ee2c553aa25f3d
goodygluck <goodygluck@gmail.com>
2026-08-31 06:01:27 -0500

    Create a custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,28 +1,9 @@
-import apiClient from '@/services/api-client';
+import useGames from '@/hooks/useGames';
 import { Text } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react'
 
-// using typescript to define the shape of the response
-interface Game {
-    id: number;
-    name: string;
-}
 
-interface fetchGameResponse {
-    count: number;
-    results: Game[];
-
-}
 const GameGrid = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
-
-    useEffect(() => {
-        // we provided a generic type argument below, with that we know the shape of the response object
-        apiClient.get<fetchGameResponse>(`/games`)
-        .then(res => setGames(res.data.results))
-        .catch(err => setError(err.message))
-    })
+   const {games, error} = useGames() 
   return (
     <>
     {error && <Text>{error}</Text>}
@@ -31,6 +12,7 @@ const GameGrid = () => {
     </>
     
   )
+
 }
 
 export default GameGrid
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,38 @@
+import apiClient from "@/services/api-client";
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+
+interface Game {
+    id: number;
+    name: string;
+}
+// using typescript to define the shape of the response
+interface fetchGameResponse {
+    count: number;
+    results: Game[];
+
+}
+
+const useGames = () => {
+
+    const [games, setGames] = useState<Game[]>([]);
+        const [error, setError] = useState('');
+    
+        useEffect(() => {
+
+            const controller = new AbortController()
+            // we provided a generic type argument below, with that we know the shape of the response object
+            apiClient.get<fetchGameResponse>(`/games`, {signal: controller.signal})
+            .then(res => setGames(res.data.results))
+            .catch(err => {
+                if (err instanceof CanceledError) return;
+                setError(err.message)})
+
+            return () => controller.abort();
+        }, [])
+
+        //   we return the below so we can use them in other components
+        return {games, error, }
+}
+
+export default useGames;
