4e1a61fbe12cc9a8dd854434d54a500814e5b041
goodygluck <goodygluck@gmail.com>
2026-09-10 21:49:43 -0500

    Handle games with no Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,9 @@
+import noImage from '@/assets/no-image-placeholder.webp';
 
 // getting optimized images to avoid loading that takes time
 const getCroppedImageUrl = (url: string) => {
-    if(!url) return '';
+    // becuase it is a static asset we want to deploy with the app, we can import it and use it as a fallback image when the url is null or undefined, instead of doing ..assets
+    if(!url) return noImage;
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
