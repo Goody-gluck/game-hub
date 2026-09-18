6b7cbe7b6a8a5c7c395c138ab8cb78d685858cc9
goodygluck <goodygluck@gmail.com>
2026-09-17 22:48:05 -0500

    Ship genres with the App

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import genres from '@/data/genres';
 
 export interface Genre {
     id: number;
@@ -7,6 +7,6 @@ export interface Genre {
 }
 
 
-const useGenre = () => useData<Genre>(`/genres`)
+const useGenre = () => ({data: genres, isLoading: false, error: null}) // this is a temporary solution to avoid the error of useGenre being undefined, we will fix this later and because we dont want the consumers of the hook to know that we are using a temporary solution, we will return the data in the same shape as the useData hook, so that the consumers of the hook can use it without knowing that we are using a temporary solution
 
-export default useGenre;
+export default useGenre; 
